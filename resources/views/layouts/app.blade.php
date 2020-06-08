9cf7f42aa8a52af64a7bb85f2ec00c03c34ba35f
sawu2019 <jennings.sawu@etu.unilim.fr>
2020-06-08 13:17:52 +0100

    fixed footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <link href="{{asset('build/css/custom.min.css')}}" rel="stylesheet">
   </head>
 
-  <body class="nav-md">
+  <body class="nav-md footer_fixed">
     <div class="container body">
       <div class="main_container">
         <div class="col-md-3 left_col">
